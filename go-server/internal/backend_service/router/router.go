dde9858ba4b4f0e894767c4e559fe618d1c4e9e5
Cilang <cilanguser@gmail.com>
2025-05-28 17:28:00 +0800

    feat(backend_service): 添加 Swagger 文档支持
    - 新增 docs包用于生成 Swagger 文档
    - 在 router 中添加 Swagger 路由
    - 生成 swagger.json 和 swagger.yaml 文档文件

--- go-server/internal/backend_service/router/router.go
+++ go-server/internal/backend_service/router/router.go
@@ -2,36 +2,25 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"tsc/internal/backend_service/interceptor"
+
+	// 导入生成的 docs 包（确保已经执行过 swag init）
+	_ "tsc/internal/backend_service/docs" // 替换为你的项目模块路径
 )
 
 func RegisterRoutes(r *gin.Engine) {
-
 	// 注册中间件
 	r.Use(
 		gin.Logger(),   // 内置日志中间件
 		gin.Recovery(), // 异常恢复
 	)
-
 	r.Use(interceptor.AuthMiddleware())
 
-	//// 健康检查
-	//r.GET("/health", func(c *gin.Context) {
-	//	c.JSON(200, gin.H{
-	//		"status": "OK",
-	//		"config": gin.H{
-	//			"ip":    serverConfig.IP,
-	//			"port":  serverConfig.Port,
-	//			"debug": serverConfig.Debug,
-	//		},
-	//	})
-	//})
-	//
-	//// 示例API
-	//r.GET("/api/v1/hello", func(c *gin.Context) {
-	//	c.JSON(200, gin.H{
-	//		"message": "Hello World",
-	//		"secret":  serverConfig.SecKey,
-	//	})
-	//})
+	// 添加 Swagger 路由
+	// 注意：如果不需要认证可以访问，可以放在 AuthMiddleware 之前
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	// 其他路由...
 }
