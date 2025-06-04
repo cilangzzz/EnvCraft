7b0c0dad4385196936c03d37791ecc4e2c464c6c
Cilang <cilanguser@gmail.com>
2025-06-04 17:32:09 +0800

    refactor(server): 重构 server模型
    - 将 DownloadInfo 和 PackageInfo 从 ServerConfig 结构体中分离，创建独立的模型
    - 更新导入路径，使用更具描述性的包名
    -为新模型添加 BaseEntity 结构，可能需要在其他地方补充相关定义

--- go-server/internal/backend_service/model/server/server.go
+++ go-server/internal/backend_service/model/server/server.go
@@ -2,8 +2,8 @@ package server
 
 import (
 	"tsc/internal/backend_service/model/common"
-	downloader "tsc/pkg/util/downloader/core"
-	server_ini "tsc/pkg/util/server_ini/core"
+	downloaderCore "tsc/pkg/util/downloader/core"
+	serverIniCore "tsc/pkg/util/server_ini/core"
 )
 
 // ServerConfig 下载任务模型
@@ -15,6 +15,20 @@ type ServerConfig struct {
 	Tag        string `json:"tag" gorm:"size:255;comment:任务标签"`
 
 	// 文件信息
-	DownloadInfo downloader.DownloadInfo `json:"download_info" gorm:"type:json;comment:下载信息"`
-	PackageInfo  server_ini.PackageInfo  `json:"package_info" gorm:"type:json;comment:文件信息"`
+	DownloadInfoId string `json:"download_info_id" gorm:"type:json;comment:下载信息"`
+	PackageInfoId  string `json:"package_info_id" gorm:"type:json;comment:文件信息"`
+}
+
+// ServerDownloadInfo 下载信息模型
+type ServerDownloadInfo struct {
+	common.BaseEntity
+	// 文件信息
+	DownloadInfo downloaderCore.DownloadInfo `json:"download_info" gorm:"type:json;comment:下载信息"`
+}
+
+// ServerPackageInfo 文件信息模型
+type ServerPackageInfo struct {
+	common.BaseEntity
+	// 文件信息
+	PackageInfo serverIniCore.PackageInfo `json:"package_info" gorm:"type:json;comment:文件信息"`
 }
